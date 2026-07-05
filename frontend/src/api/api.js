9043521a8b3aa1b38775946828d46c67324b15d9
vyshnavi7-coder <chinthalavyshnavi7@gmail.com>
2026-07-05 17:03:55 +0530

    Updated production API URLs

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://careerbridge-backend-65et.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
--- frontend/src/api/resume.js
+++ frontend/src/api/resume.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const resumeAPI = axios.create({
-  baseURL: "http://localhost:5000/api/resume",
+  baseURL: "https://careerbridge-backend-65et.onrender.com/api/resume",
 });
 
 export default resumeAPI;
